47cef72c4fb6fb5389eec3fc12dd9490ea4537ee
Oliver Sánchez <oliver-mbp@Olivers-MBP.attlocal.net>
2023-05-25 00:16:19 -0500

    design changes

--- src/app/pages/comment/comment.component.ts
+++ src/app/pages/comment/comment.component.ts
@@ -9,26 +9,10 @@ import { CommentService } from '../../core/services/comment/comment.service';
   styleUrls: ['./comment.component.css']
 })
 export class CommentComponent implements OnInit {
-  model: Comment;
-  submitted = false;
-  comments: any = []
 
    constructor(private commentService: CommentService) { }
   
-   add(newComment: Comment): void {
-      if(!newComment) {return;}
-      this.commentService.addComment(newComment)
-      .subscribe(comment => {
-        this.comments.push(comment);
-      });
-  }
-  
   ngOnInit() {
-    this.model = new Comment('','');
-    this.comments = this.commentService.getComments();
-    this.comments.forEach(element => {
-      console.log(JSON.stringify(element));
-    });
   }
 
 }
